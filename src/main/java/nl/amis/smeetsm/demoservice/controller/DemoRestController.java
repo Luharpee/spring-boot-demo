4dad8d2dd0fa8e56b7c337f2e905e656b42d9cb1
dsfsdfsdfsdf <rdppatel124@gmail.com>
2025-10-10 15:19:47 +0530

    commiting new changes

--- src/main/java/nl/amis/smeetsm/demoservice/controller/DemoRestController.java
+++ src/main/java/nl/amis/smeetsm/demoservice/controller/DemoRestController.java
@@ -10,8 +10,5 @@ public class DemoRestController {
     //If you want XML, then add a wrapper class as Root XML element, for example EmployeeList
     public String demoReply() {
         return "Hi there";
-    }
-
-
-    
+    }    
 }    
